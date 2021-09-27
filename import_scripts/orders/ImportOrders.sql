7db2d61a4d58a3dc125992e5ce8ac326e2f72f13
Koffiemolen <bastiaan.clement@gmail.com>
2021-09-27 19:10:54 +0200

    tadaa

---
+++ import_scripts/orders/ImportOrders.sql
@@ -0,0 +1,13 @@
+ SELECT * FROM new_schema.marioorderdata01;
+ 
+# ID, OrderStatus, DeliveryPlan, DeliveryTime, DeliveryPrice, Customer_Id, DeliveryType_ID, Shop_ID, OrderDate, TotalOrderPrice, CreatedOn, CreatedBy, LastUpdate, UpdateBy, Deleted
+# ID, OrderStatus, DeliveryPlan, DeliveryTime, DeliveryPrice, Customer_Id, DeliveryType_ID, Shop_ID, OrderDate, TotalOrderPrice, CreatedOn, CreatedBy, LastUpdate, UpdateBy, Deleted
+
+ 
+ INSERT INTO marios_pizza.order(ID, OrderStatus, DeliveryPlan, DeliveryTime, DeliveryPrice, Customer_Id, DeliveryType_ID, Shop_ID, OrderDate, TotalOrderPrice, CreatedOn, CreatedBy, LastUpdate, UpdateBy, Deleted)
+ VALUES(
+		 (),
+         (),
+         (),
+         () 
+ );
