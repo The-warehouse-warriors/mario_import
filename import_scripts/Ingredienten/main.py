93e3e47723b4cfa379327e16502a625c1ddf8391
Harm van Genesen <harmvangenesen@gmail.com>
2021-09-29 15:15:09 +0200

    Script aangevult
    Script aangevuld met: 
    - gemiddelde verwerkingstijd
    - log opgesplist in skiplog en insertlog
    - skiplog (alle inserts die worden geskipt komen hier te staan) 
    - insertlog (alle inserts komen hier in te staan)
    - extra validatie op "$" tekens gezien deze prijsen niet worden gehanteerd. Dit komt in de skiplog te staan. 
    - cleanup overbodige text/print()
    
    TODO: 
    - omschrijven wat alle functies doen.

--- import_scripts/Ingredienten/main.py
+++ import_scripts/Ingredienten/main.py
@@ -14,7 +14,13 @@ try:
     table = 'ingredienten'
     user = "System"
     timeStamp = datetime.datetime.now()
-    log = []
+    starttime = datetime
+    endtime = datetime
+    processtime = datetime
+    insertlog = []
+    skiplog = []
+    timelog = []
+
 
     if mariosDB.is_connected():
 
@@ -26,11 +32,6 @@ try:
 except Error as e:
     print('--- Error while connecting to database ---', e)
 
-#   Bij grote bestanden kan er ook gebruik worden gemaakt van een class. Voor de ingredienten is dit misschien niet nodig
-class Ingredient:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
 
 def removeCharInName(text):
     characters_to_remove_in_name = "!()@€[], '"
@@ -38,12 +39,18 @@ def removeCharInName(text):
         text = text.replace(ch, "")
     return text
 
-def removeCharInPrice(text):
-    characters_to_remove_in_price = "!()@€[], 'qwertyuiopasdfghjklzxcvbnm<>$"
-    for ch in characters_to_remove_in_price:
-        text = text.replace(ch, "")
 
+def removeCharInPrice(text):
+    wrongCharacters = "$"
+    charactersToRemove = "!()@€[], 'qwertyuiopasdfghjklzxcvbnm<>-"
+    for wch in wrongCharacters:
+        if wch in text != True:
+            text = False
+        else:
+            for ch in charactersToRemove:
+                text = text.replace(ch, "")
     return text
+
 
 def CheckIngredientExists(ingredient):
     sql = "SELECT Name FROM {table} WHERE Name = '{IngredientName}'".format(
@@ -55,11 +62,10 @@ def CheckIngredientExists(ingredient):
     result = cursor.fetchall()
 
     if len(result) == 0:
-        print(ingredient, " does not exists in database")
         return False
     else:
-        print("Ingredient", ingredient, "already exsists")
         return True
+
 
 def TaxImport():
     sql = "INSERT INTO tax (Tax, Description , CreatedOn, CreatedBy, LastUpdate, UpdateBy ) VALUES (%s, %s, %s, %s, %s, %s)"
@@ -67,48 +73,84 @@ def TaxImport():
     cursor.execute(sql, val)
 
     mariosDB.commit()
-    print(cursor.rowcount, "record inserted")
+    print("tax inserted")
     sql = "INSERT INTO tax (Tax, Description , CreatedOn, CreatedBy, LastUpdate, UpdateBy ) VALUES (%s, %s, %s, %s, %s, %s)"
     val = (21, "21% BTW", timeStamp, user, timeStamp, user)
     cursor.execute(sql, val)
 
     mariosDB.commit()
-    print(cursor.rowcount, "record inserted")
+    print("tax inserted")
 
 
 def IngredientImport():
+    # start tijd word bepaald
+    starttime=datetime.datetime.now()
 
-#   Data kan ik de while loop worden geimporteert.
+    print("-- Start inport --")
 
+    # data word geïmporteerd naar een array.
     df = pd.read_csv (r'C:\Users\harmv\OneDrive\Bureaublad\Extra Ingredienten.csv')
     arrayData = df.values
     arrayLen = len(arrayData)
+
+    # Aanmaken van while loop voor het inport process
     i = 0
     while i < arrayLen:
-        string1 = str(arrayData[i])
-        name1,price1=string1.split(";")
-        name1 = removeCharInName(name1)
-        price1 = removeCharInPrice(price1)
-        price1 = float(price1)
+        # naam en prijs worden gesplitst om in de juiste row te kunnen worden geïmporteerd.
+        rawstring = str(arrayData[i])
+        rawname,rawprice=rawstring.split(";")
 
-        Check = CheckIngredientExists(name1)
+        cleanprice = removeCharInPrice(rawprice)
+        if cleanprice == False:
+            logInsert = "Insert {} skipt because of wrong value in price. ({})"
+            skiplog.append(logInsert.format(i + 1, rawprice))
+            continue
+            i+=1
+
+        cleanprice = float(cleanprice)
+        cleanname = removeCharInName(rawname)
+
+        # controle in database voor het bestaan van de item
+        Check = CheckIngredientExists(cleanname)
 
         if Check == True:
+            logInsert = "input: {} with name: {} and price: €{} is not inserted: already exists"
+            skiplog.append(logInsert.format(i + 1, cleanname, cleanprice))
             i+=1
             continue
         else:
             sql = "INSERT INTO ingredient (Name, Price, Tax_ID, CreatedOn, CreatedBy, LastUpdate, UpdateBy ) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-            val = (name1, price1, 1, timeStamp, user, timeStamp, user)
+            val = (cleanname, cleanprice, 1, timeStamp, user, timeStamp, user)
             cursor.execute(sql, val)
             mariosDB.commit()
-            logInsert = "{} time {} is inserted"
-            log.append(logInsert.format(cursor.rowcount, name1))
+            logInsert = "input: {} with name: {} and price: €{} is inserted"
+            insertlog.append(logInsert.format(i+1, cleanname, cleanprice))
             i+=1
+    # stelt de eind tijd van de inport vast
+    endtime = datetime.datetime.now()
+    processtime = endtime-starttime
+    print("-- End inport --")
+    print("")
+    print("Total time consumed: ", processtime)
 
 if __name__ == '__main__':
+    testprijs = "$1.50,-"
+    check = removeCharInPrice(testprijs)
+    if check == False:
+        logInsert = "Insert test skipt because of wrong value in price. ({})"
+        skiplog.append(logInsert.format(testprijs))
+    else:
+        logInsert = "testprijs is door gekomen. ({})"
+        insertlog.append(logInsert.format(testprijs))
+
     TaxImport()
     print(" ")
     IngredientImport()
-
-    print(log)
-
+    print("")
+    print("insertlog:")
+    for i in insertlog:
+        print(i)
+    print("")
+    print("skiplog:")
+    for x in skiplog:
+        print(x)
