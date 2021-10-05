3673da50a99ee478bf3c75d7fd599946d8f7bdba
Snixies <79220714+Snixies@users.noreply.github.com>
2021-10-05 21:43:11 +0200

    make and fill temporary table

--- import_scripts/pizzabodems/.idea/pizzabodems.py
+++ import_scripts/pizzabodems/.idea/pizzabodems.py
@@ -1,37 +1,72 @@
-#import pandas as pd
-#import mysql.connector
+import pandas as pd
+import mysql.connector
 from openpyxl import load_workbook
 
-#df = pd.read_excel('pizzabodems.xlsx')
-#print(df.head())
+df = pd.read_excel('pizzabodems.xlsx')
+print(df.to_string())
+table = "temporaryPizzaBottoms"
 
-# mydb = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="televisie",
-#     database="marios_pizza"
-# )
 
-#bottomTable = 'pizzabottom'
-filename = load_workbook('pizzabodems.xlsx')
-sheet = filename.active
-rows = sheet.rows
-
-def readBottom():
-    headers = [cell.value for cell in next(rows)]
-    print(headers)
-
+def createDbConnector():
     try:
-        rowdata = next(rows)
+        global mydb
+        mydb = mysql.connector.connect(
+            host="localhost",
+            port="330",
+            user="root",
+            password="televisie",
+            database="marios_pizza"
+        )
+        global cursor
+        cursor = mydb.cursor()
+    except:
+        print("Kan database niet bereiken.")
 
-        while rowdata != "":
-            data = [cell.value for cell in rowdata]
-            if data == "":
-                break
-            print(data)
-            rowdata = next(rows)
-    except StopIteration:
-        print("Alle data is ontvangen")
+def createTable():
+    try:
+        query = "create  table " + table + "(name varchar(254), diameter varchar(254), omschrijving varchar(254), toeslag varchar(254), beschikbaar varchar(254))"
+        cursor.execute(query)
+    except:
+        cleanQuery = "drop table " + table
+        cursor.execute(cleanQuery)
+        createTable()
+
+def fillTable():
+    try:
+        for (row,rs) in df.itterrows():
+            naam = rs[0]
+            diameter = rs[1]
+            omschrijving = rs[2]
+            toeslag = rs[3]
+            beschikbaar = rs[4]
+            query = "insert into " + table + "(naam, diameter, omschrijving, toeslag, beschikbaar) values (%s, %s, %s, %s, %s)"
+            val = (naam, diameter, omschrijving, toeslag, beschikbaar)
+            cursor.execute(query, val)
+            mydb.commit()
+            print("Filled table: " + table)
+    except:
+        print("Can't fill table: " + table + "!")
+
+# bottomTable = 'pizzabottom'
+# filename = load_workbook('pizzabodems.xlsx')
+# sheet = filename.active
+# rows = sheet.rows
+#
+# def readBottom():
+#     headers = [cell.value for cell in next(rows)]
+#     print(headers)
+#
+#     try:
+#         rowdata = next(rows)
+#
+#         while rowdata != "":
+#             data = [cell.value for cell in rowdata]
+#             if data == "":
+#                 break
+#             print(data)
+#             rowdata = next(rows)
+#     except StopIteration:
+#         print("Alle data is ontvangen")
 
 #def importBottom(filename):
 
@@ -42,6 +77,10 @@ def readBottom():
 #
 #     print("--- Handle Record --")
 #     print('Pizzabodem: ' + line)
+#
+createDbConnector()
+createTable()
+fillTable()
+mydb.close()
 
-readBottom()
 
