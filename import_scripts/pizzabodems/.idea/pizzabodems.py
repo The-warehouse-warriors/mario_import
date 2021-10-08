bc60929cf0f2f66897f88d88a2eb58e8aae4e06d
Snixies <79220714+Snixies@users.noreply.github.com>
2021-10-08 11:23:59 +0200

    To fill the flat table

--- import_scripts/pizzabodems/.idea/pizzabodems.py
+++ import_scripts/pizzabodems/.idea/pizzabodems.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import mysql.connector
+from mysql.connector import Error
 # import pymysql
 from openpyxl import load_workbook
 
@@ -21,12 +22,12 @@ def createDbConnector():
         )
         global cursor
         cursor = mydb.cursor()
-    except:
-        print("Kan database niet bereiken.")
+    except Error as e:
+        print("Kan database niet bereiken." + e)
 
 def createTable():
     try:
-        query = "create  table " + table + "(name varchar(254), diameter varchar(254), omschrijving varchar(254), toeslag varchar(254), beschikbaar varchar(254))"
+        query = "create  table " + table + "(naam varchar(254), diameter varchar(254), omschrijving varchar(254), toeslag varchar(254), beschikbaar varchar(254))"
         cursor.execute(query)
     except:
         cleanQuery = "drop table " + table
@@ -34,53 +35,16 @@ def createTable():
         createTable()
 
 def fillTable():
-    cols = "`,`".join([str(i) for i in df.columns.tolist()])
+    cols = ",".join([str(i) for i in df.columns.tolist()])
     try:
-        for (i,row) in df.itterrows():
-            # naam = rs[0]
-            # diameter = rs[1]
-            # omschrijving = rs[2]
-            # toeslag = rs[3]
-            # beschikbaar = rs[4]
-            query = "insert into '" + table + "'('" + cols + "') values (" + "%s,"*(len(row)-1) + "%s)"
-            # val = (naam, diameter, omschrijving, toeslag, beschikbaar)
+        for (i,row) in df.iterrows():
+            query = "insert into " + table + "(" + cols + ") values (" + "%s,"*(len(row)-1) + "%s)"
             cursor.execute(query, tuple(row))
-            mydb.commit()
-            print("Filled table: " + table)
-    except:
-        print("Can't fill table: " + table + "!")
+        mydb.commit()
+        print("Filled table: " + table)
+    except Error as e:
+        print(e)
 
-# bottomTable = 'pizzabottom'
-# filename = load_workbook('pizzabodems.xlsx')
-# sheet = filename.active
-# rows = sheet.rows
-#
-# def readBottom():
-#     headers = [cell.value for cell in next(rows)]
-#     print(headers)
-#
-#     try:
-#         rowdata = next(rows)
-#
-#         while rowdata != "":
-#             data = [cell.value for cell in rowdata]
-#             if data == "":
-#                 break
-#             print(data)
-#             rowdata = next(rows)
-#     except StopIteration:
-#         print("Alle data is ontvangen")
-
-#def importBottom(filename):
-
-#Handle bottomdata
-# def handleRecords(line):
-#     if line == "":
-#         return
-#
-#     print("--- Handle Record --")
-#     print('Pizzabodem: ' + line)
-#
 createDbConnector()
 createTable()
 fillTable()
