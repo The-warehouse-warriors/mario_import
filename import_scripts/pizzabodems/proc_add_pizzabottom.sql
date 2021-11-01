62de002bd7dc2ddf4c03fc4388af63c7d1783d71
Snixies <79220714+Snixies@users.noreply.github.com>
2021-11-01 17:18:18 +0100

    Fixed add bottom

--- import_scripts/pizzabodems/proc_add_pizzabottom.sql
+++ import_scripts/pizzabodems/proc_add_pizzabottom.sql
@@ -1,5 +1,5 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_add_pizzabottom`(IN _name varchar(45), _size varchar(45), _description varchar(254), _price decimal(7,3), available tinyint)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `proc_add_pizzabottom`(IN _name varchar(45), _size varchar(45), _description varchar(254), _price decimal(7,3), _available tinyint)
 BEGIN
-INSERT INTO pizzabottom (Name, Size, Descrition, Active, Price, Tax_ID, CreatedOn, CreatedBy, LastUpdate, UdateBy, Deleted)
+INSERT INTO pizzabottom (Name, Size, Description, Active, Price, Tax_ID, CreatedOn, CreatedBy, LastUpdate, UpdateBy, Deleted)
 		VALUES(_name, _size, _description, _available, _price, 9, CURRENT_TIMESTAMP, 'System', CURRENT_TIMESTAMP, 'System', 0);
 END
